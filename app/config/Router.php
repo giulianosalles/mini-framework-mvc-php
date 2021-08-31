15f011c743e93cb801a8e916ec54cf0160abf50e
Giuliano Salles <giulianosalles33@gmail.com>
2021-08-31 19:54:40 -0300

    Criando o sistema de rotas simples para chamar as rotas por funções

--- app/config/Router.php
+++ app/config/Router.php
@@ -1,2 +1,13 @@
 <?php
 
+$this->get('/', function(){
+    echo 'HOME!!!';
+});
+
+$this->get('/home/', function(){
+    echo 'Estou na HOME!!!';
+});
+
+$this->get('/about/test', function(){
+    echo 'Estou na About Test!!!';
+});
--- public/index.php
+++ public/index.php
@@ -2,10 +2,8 @@
 
 require_once('../vendor/autoload.php');
 require_once('../app/functions/functions.php');
-
-use app\controller\TesteController;
+require_once('../app/config/config.php');
 
 (new \app\core\RouterCore());
 
-$controller = new TesteController();
 
